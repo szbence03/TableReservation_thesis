a82b3a5f2596ad4e2893a5a5b7c9f2082194cbe2
szbence03 <szilagyi.bence2003@gmail.com>
2025-12-29 16:38:35 +0100

    További funkciók, alkalmazás továbbfejlesztése

--- src/main/java/com/asztalfoglalas/asztalfoglalas/dao/AsztalRepository.java
+++ src/main/java/com/asztalfoglalas/asztalfoglalas/dao/AsztalRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 public interface AsztalRepository extends JpaRepository<Asztal, Integer> {
 
-    @Query("SELECT a FROM Asztal a WHERE a.id NOT IN (SELECT f.asztal.id FROM Foglalas f WHERE mettol <= ?2 AND meddig >= ?1)")
+    @Query("SELECT a FROM Asztal a WHERE a.id NOT IN (SELECT f.asztal.id FROM Foglalas f WHERE f.mettol <= ?2 AND f.meddig >= ?1)")
     List<Asztal> getSzabadAsztalok(LocalDateTime mettol, LocalDateTime meddig);
 
-    @Query("SELECT a FROM Asztal a WHERE a.id IN (SELECT f.asztal.id FROM Foglalas f WHERE mettol <= ?2 AND meddig >= ?1)")
+    @Query("SELECT a FROM Asztal a WHERE a.id IN (SELECT f.asztal.id FROM Foglalas f WHERE f.mettol <= ?2 AND f.meddig >= ?1)")
     List<Asztal> getFoglaltAsztalok(LocalDateTime mettol, LocalDateTime meddig);
 
 
